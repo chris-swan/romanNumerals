34856b53f9c391d39dec2bd0bb179e44ffe68123
steve smietana & chris swan <student@epicodus.com>
2015-09-10 15:35:38 -0700

    complete tests and project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
-var countBy = function(total, multiple) {
-
-    var result = [];
-
-    if(total < multiple) {
-        alert("Please enter a multiple lower than the total");
-        document.write("Please try again with a lower count by value!");
-    } else {
-        for (var i = 1; i*multiple <= total; i++) {
-            result.push(i*multiple);
-        }
-        return result;
+var romans = function(userNumber) {
+  var number = userNumber;
+  if (userNumber > 3999) {
+    alert("Please enter a number lower than 4,000");
     }
-}
+    else {
+      var values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+      var symbols = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+      var results = [];
+      var i = 0;
+      var count = 0;
 
-    $(document).ready(function() {
-      $("form#countBy").submit(function(event) {
-        var total = $("input#total").val();
-        var multiple= $("input#multiple").val();
-        var result = countBy(total, multiple);
-        $(".result").append("<li>" + String(result) + "</li>");
+      while(number > 0) {
+        while(i < values.length) {
+              if(number >= values[i]) {
+                  results.push(symbols[i]);
+                  number = number - values[i];
+                  count++;
+                  console.debug(results);
+              } else {
+                  i++;
+                  count = 0;
+              }
+        }
+      }
+          var str = "";
+          for (var j = 0;
+            j< results.length;
+            j++) {
+            str+=results[j]+"";
+          }
+         return str;
+     }
+  }
 
-        event.preventDefault();
-     });
+  $(document).ready(function() {
+    $("form#RomanCount").submit(function(event) {
+      var total = $("input#userNumber").val();
+      var results = romans(total);
+      $(".result").append("<li>" + String(results) + "</li>");
+
+      event.preventDefault();
+   });
 
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,9 @@
-describe('count by', function() {
-	it("counts up to a given first number in multiples of secondary number provided by user", function () {
-		expect(countBy(10, 2)).to.eql([2, 4, 6, 8, 10])
+describe('Roman Numerals', function() {
+	it("translates numeric number into roman numerals", function () {
+		expect(romans(10)).to.eql(['X']);
+	});
+
+	it("is anble to adjust for no more than 3 characters in a row", function () {
+		expect(romans(19)).to.eql(['X','I','X']);
 	});
 });
