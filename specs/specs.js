f96344682e043d35128b1c254a1057fbb9c858a2
steve smietana & chris swan <student@epicodus.com>
2015-09-10 16:49:15 -0700

    fix syntax in tests.

--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,9 @@
 describe('Roman Numerals', function() {
 	it("translates numeric number into roman numerals", function () {
-		expect(romans(10)).to.eql(['X']);
+		expect(romans(10)).to.eql('X');
 	});
 
-	it("is anble to adjust for no more than 3 characters in a row", function () {
-		expect(romans(19)).to.eql(['X','I','X']);
+	it("is able to adjust for no more than 3 characters in a row", function () {
+		expect(romans(19)).to.eql('XIX');
 	});
 });
